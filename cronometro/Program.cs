928f1fe3fab9ef1e88f8d7e75fa6a351110faca4
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-08-17 14:06:33 -0300

    Calculadora feita
    ainda falta terminar a opção de saída.

--- cronometro/Program.cs
+++ cronometro/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace MeuApp
+﻿namespace MeuApp
 {
     class Program
     {
@@ -11,16 +9,24 @@ namespace MeuApp
         static void Menu()
         {
             Console.WriteLine("S - Segundos -> 10s = 10 segundos");
-            Console.WriteLine("M  - Minutos -> 1m = 1 minuto");
+            Console.WriteLine("M - Minutos -> 1m = 1 minuto");
             Console.WriteLine("0 - Sair");
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length - 1, 1));
             int contador = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplicador = 1;
             
             Console.WriteLine(data);
             Console.WriteLine(type);
             Console.WriteLine(contador);
 
+            if (type == 0)
+                System.Environment.Exit(0);
+            if (type == 's')
+                multiplicador = 1;
+            if (type == 'm')
+                multiplicador = 60;
+            Start(contador * multiplicador);
         }
         static void Start(int contador)
         {
@@ -33,6 +39,7 @@ namespace MeuApp
                 Thread.Sleep(1000);
             }
             Console.WriteLine("Fim!");
+            Menu();
         }
     }
 }

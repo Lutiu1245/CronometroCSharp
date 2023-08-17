b1e93a8ffaeaa48c4e11f0e51061c5bd3ed04f99
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-08-17 13:50:14 -0300

    Contador e parte do menu de terminal feito.

--- cronometro/Program.cs
+++ cronometro/Program.cs
@@ -4,6 +4,35 @@ namespace MeuApp
 {
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.WriteLine("S - Segundos -> 10s = 10 segundos");
+            Console.WriteLine("M  - Minutos -> 1m = 1 minuto");
+            Console.WriteLine("0 - Sair");
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int contador = int.Parse(data.Substring(0, data.Length - 1));
+            
+            Console.WriteLine(data);
+            Console.WriteLine(type);
+            Console.WriteLine(contador);
+
+        }
+        static void Start(int contador)
+        {
+            int contadorAtual = 0;
+            while (contadorAtual != contador)
+            {
+                Console.Clear();
+                contadorAtual++;
+                Console.WriteLine(contadorAtual);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Fim!");
+        }
     }
 }
